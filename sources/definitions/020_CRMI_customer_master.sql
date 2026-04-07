025a9c9ed8ceb76ecc5d413856f94c3abdb324c0
sfc-gh-mdaeppen <marcel.daeppen@snowflake.com>
2026-04-07 09:00:37 +0200

    Remove WITH TAG from ADDRESSES columns (tags not yet created)

--- sources/definitions/020_CRMI_customer_master.sql
+++ sources/definitions/020_CRMI_customer_master.sql
@@ -60,13 +60,12 @@ DEFINE TABLE {{ sf_db }}.{{ SCHEMA_CRM_RAW }}.CRMI_RAW_TB_CUSTOMER (
 COMMENT = 'Customer master data with SCD Type 2 support. Multiple records per customer, uniquely identified by (CUSTOMER_ID, INSERT_TIMESTAMP_UTC). Address data stored separately in CRMI_RAW_TB_ADDRESSES.';
 
 
-
 DEFINE TABLE {{ sf_db }}.{{ SCHEMA_CRM_RAW }}.CRMI_RAW_TB_ADDRESSES (
     CUSTOMER_ID VARCHAR(30) NOT NULL COMMENT 'Reference to customer (foreign key to CRMI_RAW_TB_CUSTOMER)',
-    STREET_ADDRESS VARCHAR(200) NOT NULL WITH TAG (SENSITIVITY_LEVEL='top_secret') COMMENT 'Street address (localized format)',
-    CITY VARCHAR(100) NOT NULL WITH TAG (SENSITIVITY_LEVEL='restricted') COMMENT 'City name (localized to country)',
+    STREET_ADDRESS VARCHAR(200) NOT NULL COMMENT 'Street address (localized format)',
+    CITY VARCHAR(100) NOT NULL COMMENT 'City name (localized to country)',
     STATE VARCHAR(100) COMMENT 'State/Region (where applicable for the country)',
-    ZIPCODE VARCHAR(20) NOT NULL WITH TAG (SENSITIVITY_LEVEL='restricted') COMMENT 'Postal code (country-specific format)',
+    ZIPCODE VARCHAR(20) NOT NULL COMMENT 'Postal code (country-specific format)',
     COUNTRY VARCHAR(50) NOT NULL COMMENT 'Customer country (12 EMEA countries supported)',
     INSERT_TIMESTAMP_UTC TIMESTAMP_NTZ NOT NULL COMMENT 'UTC timestamp when this address record was inserted (for SCD Type 2)',
     
